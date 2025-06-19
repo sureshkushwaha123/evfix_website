717d4ea825e8309ad8e192a2e355fb99fd85c293
sureshkushwaha123 <sp.kushawaha12@gmail.com>
2025-06-19 15:58:04 +0530

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="container bg-green-500">
+      <h1 className="text-3xl font-bold ">hello world!</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
