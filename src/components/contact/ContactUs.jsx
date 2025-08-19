637250dcea8d4bd87a6f417ac79590daa3c80056
sureshkushwaha123 <sp.kushawaha12@gmail.com>
2025-08-19 21:34:17 +0530

    map updated

--- src/components/contact/ContactUs.jsx
+++ src/components/contact/ContactUs.jsx
@@ -71,7 +71,7 @@ const ContactPage = () => {
         <div className="w-full h-[520px] rounded-2xl overflow-hidden shadow-md bg-gray-200">
           <iframe
             title="EV Garaz Location"
-            src="https://www.google.com/maps/embed?pb=..."
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3740.6873001895206!2d85.79826557429489!3d20.354532781131418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909945d29cb3b%3A0x1b6a81b09a37ff02!2sO-HUB%20(Startup%20Odisha)!5e0!3m2!1sen!2sin!4v1755619338839!5m2!1sen!2sin"
             width="600"
             height="450"
             style={{ border: 0 }}
